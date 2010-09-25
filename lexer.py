4a942f9783c9391bf9b73ca688349933068c8cf7
John Gunderman <johngunderman@gmail.com>
2010-09-25 11:01:49 -0400

    Updates to lexer and parser

--- lexer.py
+++ lexer.py
@@ -1,3 +1,10 @@
+import string
+
+
+SYMBOL = "symbol"
+NUM = "num"
+ROOT = "root"
+EXPR = "expr"
 
 
 class Lexer(object):
@@ -12,4 +19,71 @@ class Lexer(object):
         - `input_code`: a string of code
         """
         self._input_code = input_code
+
+
+    def tokenize(self):
+        tokens = [] # array of Token objects
+        temp_token = ""
         
+        for char in self._input_code:
+            if char in "()":
+                tokens.append(Token(char))
+            else if char in string.letters:
+                temp_token.append(char)
+            else if char in string.digits:
+                temp_token.append(char)
+            else if char in string.whitespace:
+                tokens.append(Token(temp_token))
+                temp_token = ""
+
+
+
+                
+            
+class Token(object):
+    """
+    data associated with a token
+    """
+    
+    def __init__(self, value):
+        """Make a new Token with given value and type
+        
+        Arguments:
+        - `value`: literal value of the token
+        """
+        self.value = value
+
+        if self.value in "()":
+            self.t_type = SYMBOL
+        if self.value[0] in string.letters:
+            self.t_type = TOKEN
+        if self.value.isdigit():
+            self.t_type = NUM
+            self.value = float(self.value)
+
+
+    def is_symbol(self, ):
+        """
+        Returns true if the  Token is of type symbol
+        """
+        return self.t_type == SYMBOL
+
+
+    def is_num(self, ):
+        """
+        Returns true if Token is of type num
+        """
+        return  self.t_type == NUM
+
+
+    def is_open(self):
+        """
+        Returns true if Token is '('
+        """
+        return self.value == '('
+
+    def is_close(self):
+        """
+        Returns true if Token is ')'
+        """
+        return self.value == ')'
--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import lexer
 
 class Parser(object):
     """
@@ -11,4 +12,59 @@ class Parser(object):
         - `tokens`: an array of tokens
         """
         self._tokens = tokens
+        self._root_node = ASTNode( None, lexer.ROOT)
+        self._current_node = self._root_node
         
+        for token in self._tokens:
+            # (, add, 1, 3, )
+            if Token.is_open():
+                new_node = ASTNode(None, lexer.EXP)
+                self._current_node.add_child(new_node)
+                self._current_node = new_node
+                
+                        
+                
+                
+            
+
+
+
+        
+
+class ASTNode(object):
+    """
+    A Node in our AST
+    """
+    
+    def __init__(self, value, n_type):
+        """
+        Generate a node with said parent, value,and type.
+        
+        Arguments:
+        - `parent`:The parent for this node
+        - `value`: The value contained by this node
+        - `n_type`:The type of hte value contained by this node
+        """
+        self._parent = parent
+        self._value = value
+        self._n_type = n_type
+        self._child_nodes = []
+
+    def add_child(self, node):
+        """
+        Adds a child to thie list of children for this node
+        
+        Arguments:
+        - `node`: The given ASTNode
+        """
+        node.set_parent_node(self)
+        self._child_nodes.append(node)
+
+        
+
+    def set_parent_node(self, node):
+        """
+        Set this ASTNodes parent to `node`
+        """
+        self._parent = node
+
